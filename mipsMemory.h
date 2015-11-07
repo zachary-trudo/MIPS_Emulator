273afe2f2451777d5060b5ce6b8a9e13071aeb03
Dustin <dustinr@uoregon.edu>
2015-11-07 15:50:06 -0800

    Started work on mipsMemory

--- mipsMemory.h
+++ mipsMemory.h
@@ -13,5 +13,15 @@ typedef struct dataMemory
     int a;
 } dataMemory;
 
+extern const int DATA_MEM_SIZE = 1028;
+extern const int INSTR_MEM_SIZE = 1028;
 
+extern int data_mem[];
+extern int instr_mem[];
+
+void save_data(int address, char* value);
+void load_data(int address);
+
+void save_program(char * program);
+void fetch_instr(int address);
 #endif
